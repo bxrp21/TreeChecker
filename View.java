e52a0234c648d2afe2e8ddaa3c060f6914e07ca4
bxrp21 <btlawrence@mta.ca>
2016-11-24 12:31:01 -0400

    hopefully finally view update

--- View.java
+++ View.java
@@ -7,22 +7,22 @@ public class View implements ViewI {
 	private Writer writer;
 	
 	public View(){
-		this(System.in,System.out);
+		this(System.in,(System.out));
 	}
 	public View(InputStream in, OutputStream out){
 		scanner = new Scanner(in);
 		writer = new PrintWriter(out);
 	}
 	
-	public String readLine(){
+	public String readLine(){		
 		return scanner.nextLine();
 	}
 	
 	public void display(Object o){
 		try{
 			if(o != null){
-				System.out.println("Display took in "+o.toString());
 				writer.write(o.toString());
+				writer.flush();
 			}
 			else{
 				writer.write(null+"\n");
