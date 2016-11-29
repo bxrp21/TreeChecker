1b73927120a37b64d33065a0cb1a30563da4f1ed
bxrp21 <btlawrence@mta.ca>
2016-11-28 22:30:34 -0400

    init test commit

--- FloweringTreeTest.java
+++ FloweringTreeTest.java
@@ -2,7 +2,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class BloomingTreeTest {
+public class FloweringTreeTest {
 
 	@Test
 	public void test() {
--- test.java
+++
@@ -1,8 +0,0 @@
-
-public class test {
-	public static void main(String[]args){
-		View v = new View();
-		v.displayPrompt();
-		v.display("Input is " + v.readLine());
-	}
-}
